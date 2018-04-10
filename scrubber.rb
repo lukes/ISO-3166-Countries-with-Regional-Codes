5867ef2e00bbd72ac957c8fa783bd85319a6c981
Luke Duncalfe <lduncalfe@eml.cc>
2018-04-10 20:11:57 +1200

    Updated scrubber.rb to work with new UN document

--- scrubber.rb
+++ scrubber.rb
@@ -16,12 +16,12 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 entities = HTMLEntities.new
 
-wikipedia_page = "https://en.wikipedia.org/wiki/ISO_3166-1"
-un_page = "http://unstats.un.org/unsd/methods/m49/m49regin.htm"
+WIKIPEDIA_URI = "https://en.wikipedia.org/wiki/ISO_3166-1"
+UN_URI = "https://unstats.un.org/unsd/methodology/m49/overview/"
 
-puts "Fetching data from Wikipedia table #{wikipedia_page}..."
+puts "Fetching data from Wikipedia table #{WIKIPEDIA_URI}..."
 
-doc = Hpricot(open(wikipedia_page).read)
+doc = Hpricot(open(WIKIPEDIA_URI).read)
 
 # array that will hold the iso 3166 data
 codes = []
@@ -39,7 +39,7 @@ doc.search("table.sortable tr").each do |row|
   country["alpha-3"] = entities.decode(tds[2].search("span").inner_html.strip) rescue nil
   country["country-code"] = entities.decode(tds[3].search("span").inner_html.strip) rescue nil
   country["iso_3166-2"] = entities.decode(tds[4].search("a").inner_html.strip) rescue nil
-  codes << country unless country.values.any?{ |v| v.nil? }
+  codes << country unless country.values.any?(&:blank?)
 end
 
 puts "  Data for #{codes.size} countries found\n"
@@ -49,92 +49,38 @@ puts "  Data for #{codes.size} countries found\n"
 
 # note ISO doesn't give away all information for free - so refer to the Wikipedia table (assuming it is mostly kept up to date)
 
-puts "Fetching data from UN table #{un_page}..."
+puts "Fetching data from UN table #{UN_URI}..."
 
-#doc = Hpricot(open(un_page).read.force_encoding("UTF-8"))
-#doc = Nokogiri::XML(open(un_page).read)
-doc = Nokogiri::HTML(open(un_page))
+doc = Nokogiri::HTML(open(UN_URI));
 
-region_name = nil
-region_code = nil
-sub_region_name = nil
-sub_region_code = nil
-found_table = false
+doc.css("table#downloadTableEN tbody").css("tr").each do |row|
 
-#doc.css("table [text()*=Numerical code]")[0].css("tr").each do |row|
-doc.css("table[width='100%']")[2].css("tr").each do |row|
+  _, _, region_code, region_name, sub_region_code, sub_region_name, _, _, country, _, iso_alpha_3 = row.css("td").map{|td| td.inner_html.strip }
 
-  # table has more sections than we want, like row
-  # of "Developed and developing regions" code. look for
-  # the next instance of td.header2 after we've started
-  # finding results, and end the loop when found
-  if !row.css("td.cheader2").blank? && found_table
-    break
-  end
-
-  tds = row.css("td")
-
-  next if tds[0].blank?
-
-  # get the code number
-  code = tds[0].css("p")[0].try(:inner_html).try(:strip)
-  code = tds[0].css("p span").inner_html.strip if code.blank?
-  code = tds[0].inner_html.strip if code.blank? # certain codes aren't wrapped in a <p>
-  next unless code.match(/^\d+\Z/)
-
-  # detemine what kind of row this is
-  # is this a region row?
-  region = tds[1].css("h3 b")
-  unless region.blank?
-    region.css("a").remove # remove the empty <a>
-    unless region.css("span").blank?
-      region = region.css("span") # remove wayward <span> (appearing on first Africa result)
-    end
-    region = region.inner_html.strip
-    region = entities.decode(region)
-    unless region.nil? || region.blank?
-      found_table = true
-      region_code = code
-      region_name = region
-      puts "#{region_name}: #{region_code}"
-      next
+  # find this country in our array and modify in place
+  codes.each_with_index do |element, i|
+    if element["alpha-3"] == iso_alpha_3
+      codes[i]["region"] = entities.decode(region_name)
+      codes[i]["sub-region"] = entities.decode(sub_region_name)
+      codes[i]["region-code"] = region_code
+      codes[i]["sub-region-code"] = sub_region_code
+      break
     end
   end
-  # is this a subregion row?
-  sub_region = tds[1].css("b").inner_html.strip
-  unless sub_region.blank?
-    sub_region = entities.decode(sub_region)
-    sub_region_code = code
-    sub_region_name = sub_region
-    puts "  #{sub_region_name}: #{sub_region_code}"
-    next
-  end
-  # is this a country row?
-  country = tds[1].css("p").inner_html.strip
-  country = tds[1].css("p span").inner_html.strip if country.blank?
-  country = tds[1].inner_html.strip if country.blank?
-
-  unless country.blank? || !country.match(/^[A-Z]/)
-    # find this country in our array and modify in place
-    codes.each_with_index do |element, i|
-      if element["country-code"] == code
-        codes[i]["region"] = region_name
-        codes[i]["sub-region"] = sub_region_name
-        codes[i]["region-code"] = region_code
-        codes[i]["sub-region-code"] = sub_region_code
-        break
-      end
-    end
-    country = entities.decode(country)
-    puts "    #{country}: #{code}"
-  end
-
+  # puts "\t#{entities.decode(country)}: #{iso_alpha_3}"
 end
 
 # For ISO data from the Wikipedia page that we couldn't correlate with
 # regional codes from the UN, give them blank data.
-codes.select{ |c| c["region-code"].nil? }.map do |c|
+blanks = codes.select do |c|
+  c.slice("alpha-3", "region", "sub-region", "region-code", "sub-region-code").values.any?(&:blank?) ||
+  !["alpha-3", "region", "sub-region", "region-code", "sub-region-code"].all? {|k| c.key?(k) }
+end
+
+# Ensure they have all the keys (we'll write them to the data files with blank values)
+blanks.map! do |c|
   c["region"] = c["sub-region"] = c["region-code"] = c["sub-region-code"] = nil
+  c
 end
 
 puts "Writing files..."
@@ -179,8 +125,11 @@ File.open("slim-3/slim-3.json", "w:UTF-8") { |f| f.write(json) }
 File.open("slim-3/slim-3.csv", "w:UTF-8") { |f| f.write(json_to_csv(json)) }
 File.open("slim-3/slim-3.xml", "w:UTF-8") { |f| f.write(json_to_xml(json)) }
 
-puts "\nCouldn't find regional table data to save in all.csv, all.json and all.xml for the following countries (you may want to manually check #{un_page}) -- sorry!:\n\n"
+puts "Done."
 
-codes.select{ |c| c["region-code"].nil? }.each {|c| puts c.inspect }
+if blanks.present?
+  puts "\nThere was some missing data for the following countries (you may want to manually check #{UN_URI}):\n\n"
+  puts blanks.each(&:inspect)
+end
 
 File.open("LAST_UPDATED.txt", "w:UTF-8") { |f| f.write(Time.now.to_s) }
